8f3a2a80784c8423abfa8ec2979079d3c2396fa5
yam <real.yamka@gmail.com>
2026-05-15 07:09:45 -0400

    def local yamtools to show version and author

--- yamtools.lua
+++ yamtools.lua
@@ -1,2 +1,10 @@
--- simple client.log saying yamtools is loaded
-client.log(yamtools, "YamTools loaded successfully!")
+local yamtools = {
+    version = "beta 0.1",
+    author = "yam-ka",
+}
+
+local function main()
+    client.log("YamTools " .. yamtools.version .. " by " .. yamtools.author)
+end
+
+main()
