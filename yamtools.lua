2a01ab3115d16054f2358d8d084194cc0dde207e
yam <real.yamka@gmail.com>
2026-05-15 07:18:46 -0400

    added returns for antiaim tab

--- yamtools.lua
+++ yamtools.lua
@@ -3,6 +3,22 @@ local yamtools = {
     author = "yam-ka",
 }
 
+local function antiaim_builder()
+    local antaim = {
+        ui.get("antiaim", "Anti-aimbot angles", "Enabled"),
+        ui.get("antiaim", "Anti-aimbot angles", "Pitch"),
+        ui.get("antiaim", "Anti-aimbot angles", "Yaw Base"),
+        ui.get("antiaim", "Anti-aimbot angles", "Yaw"),
+        ui.get ("antiaim", "Anti-aimbot angles", "Yaw jitter"),
+        ui.get("antiaim", "Anti-aimbot angles", "Body yaw"),
+        ui.get("antiaim", "Anti-aimbot angles", "Freestanding body yaw"),
+        ui.get("antiaim", "Anti-aimbot angles", "Edge yaw"),
+        ui.get("antiaim", "Anti-aimbot angles", "Freestanding"),
+        ui.get("antiaim", "Anti-aimbot angles", "Roll"),
+    }
+    return antaim
+end
+
 local function main()
     client.log("YamTools " .. yamtools.version .. " by " .. yamtools.author)
 end
